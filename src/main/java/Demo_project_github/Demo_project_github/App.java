91cadded358b4171a5c5f3544008d74efc67b222
akshaysadar <akshaysadar007@gmail.com>
2022-03-14 09:16:19 +0530

    I have initiated project and commit first

---
+++ src/main/java/Demo_project_github/Demo_project_github/App.java
@@ -0,0 +1,13 @@
+package Demo_project_github.Demo_project_github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Demo_project_github/Demo_project_github/AppTest.java
@@ -0,0 +1,20 @@
+package Demo_project_github.Demo_project_github;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
